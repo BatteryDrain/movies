5429597b592546b9d9bbce7cce4c14465cbda097
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-09-27 17:37:40 -0600

    removing old logs

--- script.js
+++ script.js
@@ -291,9 +291,6 @@ function makeFig(place, m){
             if(place == "red"){
                 fig.style.backgroundColor = "rgb(169, 102, 102)";
             }
-            if(DATASORTED[m][1] == "Central Intelligence"){
-                console.log("here");
-            }
             cont.appendChild(fig);
 }
 

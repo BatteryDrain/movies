05d63530c7f033049e57d3c79fb6b105236cfdf8
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-10-13 14:22:25 -0600

    fixed annother error

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ function build(m) {
 
                         for(i=0; i<hasTags.length; i++){
                             for(j=0; j=TAGS.length; j++){
-                                if(TAGINUSE[j][1]==hasTags[i]){
+                                if(TAGS[j][1] == hasTags[i]){
                                     TAGCOUNT[j].push(TAGCOUNT[j] + 1);
                                 }
                             }

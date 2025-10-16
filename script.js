81f19f3faa3bf218bae9eacee2c1d7876a572a16
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-10-16 15:46:29 -0600

    adding wrap back

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ TAGCOUNT = 0;
 GROUPEDDATA = [[]];
 EXPANDED = false;
 TAGCOUNTL = [];
-COL = window.innerWidth / 400;
+COL = window.innerWidth / 300;
 COL = Math.floor(COL);
 
 filterage();
@@ -220,7 +220,7 @@ function resetF(){
 }
 
 function populate(){
-    COL = Math.floor(window.innerWidth / 400);
+    COL = Math.floor(window.innerWidth / 300);
     console.log(COL);
     placeHere.replaceChildren();
     //make columbs

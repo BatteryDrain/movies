2005e926006253f329c6b0107e87e5fdefc16758
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-10-20 07:13:39 -0600

    removed log

--- script.js
+++ script.js
@@ -298,7 +298,6 @@ function build(m, n) {
 
 
 function makeFig(place, m, n){
-    console.log("n:", n, "COL:", COL, "n % COL:", n % COL);
     colum = document.getElementById("div" + (n % COL));
     fig = document.createElement('figure');
             div = document.createElement('div');

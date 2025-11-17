eb7a6e860489841e89994ea04691369ff4750e1a
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-11-17 12:05:40 -0700

    fixed button

--- script.js
+++ script.js
@@ -364,9 +364,9 @@ function makeFig(place, m, n){
             but = document.createElement('button');
             but.innerHTML = "show";
             but.id = DATASORTED[m][0] + "show";
-            // but.classList.add("hide");
+            but.classList.add("hide");
             but.setAttribute("onclick", "saw(" + DATASORTED[m][0] + ")");
-            but.appendChild(fig);
+            fig.appendChild(but);
 
 
         fig.style.backgroundColor = `hsl(${place * 108}, ${65}%, ${58.4}%)`;

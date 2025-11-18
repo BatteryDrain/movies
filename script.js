e074893fa12781ebf02e0e3348b013e09cedaf94
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-11-18 16:14:10 -0700

    added new styles to div2

--- script.js
+++ script.js
@@ -360,6 +360,7 @@ function makeFig(place, m, n){
             but = document.createElement('button');
             but.id = DATASORTED[m][0] + "show";
             but.innerHTML = "seen it";
+            but.style.cssFloat = "left";
             but.setAttribute("onclick", "saw(" + DATASORTED[m][0] + ")");
             fig.appendChild(but);
 
